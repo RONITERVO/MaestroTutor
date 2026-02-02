56da6be4cd4ee780e60b25629e06bdd0c0ba8f9f
Roni Tervo <ronitervo.rt@gmail.com>
2026-02-02 15:45:15 +0200

    Color cue for language scroll

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === 'ghpages' ? '/MaestroTutor/' : '/',
+  publicDir: 'public',
   resolve: {
     alias: {
       '@': '/src'
